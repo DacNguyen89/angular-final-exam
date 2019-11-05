f3541dc0233ac1131cdbba1ce585fe8d58c02bee
DacNguyen89 <dac.ngm89@gmail.com>
2019-11-05 09:37:31 +0700

    generate require component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BooksComponent} from './books/books.component';
+import {CreateBookComponent} from './create-book/create-book.component';
+import {EditBookComponent} from './edit-book/edit-book.component';
+import {DeleteBookComponent} from './delete-book/delete-book.component';
+import {BookDetailComponent} from './book-detail/book-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    CreateBookComponent,
+    EditBookComponent,
+    DeleteBookComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
